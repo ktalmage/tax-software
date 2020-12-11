83046ccccdf5efb88bfad0eae5886721c93ca513
ktalmage <ktalmage85@gmail.com>
2020-12-11 14:58:04 -0500

    routes, undo devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
-    
+
+    def new
+        @user = User.new
+    end
+
+    def create
+
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username,:password,:password_confirmation)
+    end
 end
