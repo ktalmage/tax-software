ae2618d844e0bfdedf3e3bc904995708b22519ca
ktalmage <ktalmage85@gmail.com>
2020-12-14 12:25:53 -0500

    routes, controller updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,14 @@ class UsersController < ApplicationController
     end
 
     def create
-
+    @user = User.new(user_params)
+        if @user.valid?
+            @user.save
+            session[:user_id] = @user.id
+            # redirect_to '/welcome'
+        else
+            render :new
+        end
     end
 
     private
